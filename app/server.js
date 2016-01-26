0f77cbc25a1bcbfd67ae6ca565b59728aca60cdb
Roman Dutchak <dutchakdev@gmail.com>
2016-01-26 18:15:43 +0200

    fix ip address for local

--- app/server.js
+++ app/server.js
@@ -4,7 +4,7 @@ var config = require(__base + 'config');
 console.info('server process starting');
 app.set('port', (process.env.PORT || 5000));
 
-app.listen(config.express.port, function (error) {
+app.listen(config.express.port, config.express.ip, function (error) {
 	if (error) {
 		console.error('Unable to listen for connections', error);
 		process.exit(10);

0b8352db954aeef4a07539e77202de739a787599
Roman Dutchak <dutchakdev@gmail.com>
2016-01-26 15:34:29 +0200

    fixes

--- bin/importDump.sh
+++ bin/importDump.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-if [ -f $1 ];
+if [ -f $DB_USER ];
 then
-	read -p "Enter Mysql username: "  username
-	read -p "Enter Mysql Host: "  host
-	read -p "Enter Mysql DB name: "  db
-	mysql -u $username -p -h $host $db < $1
+	mysql -u $DB_USER --password $DB_PASSWORD -h $DB_HOST $DB_NAME < ./file.sql
 else
-	echo 'Usage: importDump.sh ./file.sql'
+	if [ -f $1 ];
+	then
+		read -p "Enter Mysql username: "  username
+		read -p "Enter Mysql Host: "  host
+		read -p "Enter Mysql DB name: "  db
+		mysql -u $username -p -h $host $db < $1
+	else
+		echo 'Usage: importDump.sh ./file.sql'
+	fi
 fi

ec1002b7b7e7983ab03b21a091909e802c6aace1
Roman Dutchak <dutchakdev@gmail.com>
2016-01-26 15:40:11 +0200

    add install scripts

--- bin/build.sh
+++ bin/build.sh
@@ -1,52 +1,13 @@
 #!/bin/bash
 # Скрипт инициализации проекта.
 
-# https://user.xmission.com/~emailbox/ascii_cats.htm
-echo "             *     ,MMM8&&&.            *
-                  MMMM88&&&&&    .
-                 MMMM88&&&&&&&
-     *           MMM88&&&&&&&&
-                 MMM88&&&&&&&&
-                 'MMM88&&&&&&'
-                   'MMM8&&&'      *
-          |\___/|
-          )     (             .              '
-         =\     /=
-           )===(       *
-          /     \
-          |     |
-         /       \
-         \       /
-  _/\_/\_/\__  _/_/\_/\_/\_/\_/\_/\_/\_/\_/\_
-  |  |  |  |( (  |  |  |  |  |  |  |  |  |  |
-  |  |  |  | ) ) |  |  |  |  |  |  |  |  |  |
-  |  |  |  |(_(  |  |  |  |  |  |  |  |  |  |
-  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |
-  jgs|  |  |  |  |  |  |  |  |  |  |  |  |  |"
-
-echo "##############################"
-echo "Install server packages (npm):"
-echo "##############################"
-npm install
-echo "##############################"
-echo "Install client packages (bower):"
-echo "##############################"
-bower install
-
-if [ "$1" = "import" ];
-then
-    echo "##############################"
-    echo "Import dump:"
-    echo "##############################"
-    exec bin/importDump.sh ./dump.sql
-fi
-
+exec bin/importDump.sh ./dump.sql
 echo "##############################"
 echo "Run migrations:"
 echo "##############################"
 exec bin/migrateUp.sh
 echo "##############################"
-echo "Init Donald Trump"
+echo "Init Donald Trump for Heroku"
 echo "##############################"
 echo "Done."
 echo "Now, lets play:"
